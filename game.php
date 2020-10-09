de1e08b52b5e1a06753d8e4edc8eea9be8d9c022
Adrien MALINGREY <adrien@malingrey.fr>
2020-10-09 03:04:12 +0200

    change unknow char

--- game.php
+++ game.php
@@ -88,7 +88,7 @@
         <em>grille</em> étant une suite de 81 caractères représentant la grille de gauche à droite puis de haut en bas, soit :
         <ul>
             <li>un chiffre entre 1 et 9 pour les cases connues</li>
-            <li>? pour les case vides</li>
+            <li>. pour les case vides</li>
         </ul>
         <a href=".">Cliquez ici pour générer une nouvelle grille</a>
     </body>
