7c65480bdd99392a7ad2581344c321a2bebf288b
adrien <adrien@malingrey.fr>
2023-04-22 16:14:46 +0200

    CDN

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css" rel="stylesheet" type="text/css" title="Automatique" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="alternate stylesheet" type="text/css" title="Clair" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css" rel="alternate stylesheet" type="text/css" title="Sombre" />
-<link rel="stylesheet" type="text/css" href="fonts/remixicon.css" />
+<link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <link rel="apple-touch-icon" href="thumbnail.php?size=57" sizes="57x57">
 <link rel="apple-touch-icon" href="thumbnail.php?size=114" sizes="114x114">
@@ -34,4 +34,4 @@
 <meta property="og:locale" content="fr_FR" />
 <meta property="og:site_name" content="<?=$_SERVER["HTTP_HOST"]?>" />
 <script src='js/sudoku.js'></script>
-<script src="js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
